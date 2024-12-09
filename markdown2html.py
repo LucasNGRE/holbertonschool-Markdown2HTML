8d7e084de4c52179cbeb5be169cb8f5683c1798c
LucasNGRE <lucas12negre@gmail.com>
2024-12-09 10:09:14 +0100

    start a script

---
+++ markdown2html.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+    markdown2html.py: Converts markdown files to html files.
+"""
+
+import sys
+import os
+
+
+def main():
+    """
+        Main function of the script.
+    """
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
+
+    markdown_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    if not os.path.isfile(markdown_file):
+        print(f"Missing {markdown_file}", file=sys.stderr)
+        sys.exit(1)
+
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
